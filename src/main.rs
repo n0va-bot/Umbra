c317acb28ee43f82db63434baaa51ed38d7fc949
N0\A <n0va@krzak.org>
2026-06-04 09:45:56 +0200

    VGA Text Mode

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 // This function is called on panic (yes, I'm even copying the comments)
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
-
-static MESSAGE: &[u8] = b"Fieletowy!";
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
@@ -18,14 +19,7 @@ pub extern "C" fn _start() -> ! {
     // (start, co robi, każdy widzi)
     // (koń, jaki jest, każdy widzi)
 
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in MESSAGE.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xd;
-        }
-    }
+    println!("Fieletowy!");
 
     loop {}
 }
