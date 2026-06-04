51204120146966c66454747887a56a1d5139bde5
N0\A <n0va@krzak.org>
2026-06-04 09:25:34 +0200

    A Minimal Rust Kernel

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,29 @@
 
 use core::panic::PanicInfo;
 
+// This function is called on panic (yes, I'm even copying the comments)
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+static MESSAGE: &[u8] = b"Fieletowy!";
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     // start
     // What start does everyone sees
     // (start, co robi, każdy widzi)
-    loop {}
-}
+    // (koń, jaki jest, każdy widzi)
 
-// This function is called on panic (yes, I'm even copying the comments)
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in MESSAGE.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xd;
+        }
+    }
+
     loop {}
 }
